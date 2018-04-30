ccf5a6f1d932b527058aef851c3bbd21826029fc
Denizcan Demir <denizcandemir@outlook.com>
2018-04-30 22:32:44 +0300

    oracle connection tamamlandi

--- src/com/deniz/connection/DatabaseConnection.java
+++ src/com/deniz/connection/DatabaseConnection.java
@@ -1,5 +1,17 @@
 package com.deniz.connection;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
 
+ public class DatabaseConnection {
+
+	public Connection getConnection()
+	{
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe","deniz","123");
+			return con;
+		} catch (Exception e) { e.printStackTrace(); return null;}
+	}
 }
